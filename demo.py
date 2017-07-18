1a2f5567b9822913f08053985cc3f21355958c36
Diego de Souza Oliveira <diego.so.pgm@gmail.com>
2017-07-18 10:21:36 -0300

    carregando imagem com canvas / utilizando tres frames (imagem, status, rotulos)

--- demo.py
+++ demo.py
@@ -3,10 +3,11 @@ from tkinter import messagebox
 from PIL import Image, ImageTk
 
  
-class teste_tk(tkinter.Tk):
+class teste_tk(tkinter.Frame):
 
-    def __init__(self,parent):
-        tkinter.Tk.__init__(self,parent)
+    def __init__(self,parent = None):
+        tkinter.Frame.__init__(self,parent)
+        
         self.parent = parent
         self.initialize()
 
@@ -35,27 +36,53 @@ class teste_tk(tkinter.Tk):
 
         self.top.config(menu=self.menuBar)
         ######################################
+        
+        self.fMain = tkinter.Frame(self.parent)
+        #self.fMain.pack(side = 'top')
+        self.fMain.grid(row=0, column = 0, stick='nswe')
+        #self.fMain.rowconfigure(1, weight=1)
+        #self.fMain.columnconfigure(1, weight=1)
+
+        self.fLabel = tkinter.Frame(self.parent)
+        #self.fLabel.pack(side = "right", fill = 'y')
+        self.fLabel.grid(row = 0, column = 1, stick='nswe')
+        #self.fLabel.rowconfigure(1, weight=1)
+        #self.fLabel.columnconfigure(1, weight=1)
+
+        self.fStatus = tkinter.Frame(self.parent)
+        #self.fStatus.pack(side='bottom', fill='x')
+        self.fStatus.grid(row = 1, column = 0, stick='nswe', columnspan=2)
+        #self.fStatus.rowconfigure(1, weight=1)
+        #self.fStatus.columnconfigure(1, weight=1)
 
         #imagem grande demais cobre o status (TODO: procurar como impedir)
-        self.status = tkinter.Label(text="Bla bla", bd=1,relief='sunken', anchor='w')
+        self.status = tkinter.Label(self.fStatus, text="Bla bla", bd=1,relief='sunken', anchor='w', bg='red')
         self.status.pack(side='bottom', fill='x')
 
+        self.label = tkinter.Label(self.fLabel, text="Bla bla", bd=1,relief='sunken', anchor='w', bg='green')
+        self.label.pack(side='right', fill = 'y')
 
         img = Image.open("demo_image.jpg")
         self.photo = ImageTk.PhotoImage(img)
-
         
-        #imagem não preenchendo a janela (TODO: procurar uma forma)
-        #canvas = tkinter.Canvas(self)
-        #canvas.grid(row = 0, column = 0)
-        #canvas.create_image(0,0,image=self.photo)
+        self.canvas = tkinter.Canvas(self.fMain, width=self.photo.width(), height=self.photo.height())
+        self.canvas.config(bg="blue")
+        self.canvas.image = self.photo
 
-        self.label = tkinter.Label(image=self.photo)
-        self.label.image = self.photo
-        self.label.pack()
+        #######
+        #deveria ser: 998x665
+        self.canvas.create_image(self.photo.width()/2, self.photo.height()/2, image=self.photo, tags="imgTag")
+        self.canvas.pack()
+        #self.canvas.pack(fill='both', expand=True)
+        self.canvas.tag_bind("imgTag", "<Button-1>", self.onClick)
+        self.canvas.tag_bind("imgTag", "<Motion>", self.motion)    
+        
 
-        self.label.bind("<Button-1>", self.onClick)
-        self.label.bind("<Motion>", self.motion)
+        #self.img = tkinter.Label(self.fMain, image=self.photo)
+        #self.img.image = self.photo
+        #self.img.pack()
+        #self.img.bind("<Button-1>", self.onClick)
+        #self.img.bind("<Motion>", self.motion)
         
 
     def nada(self):
@@ -79,8 +106,13 @@ class teste_tk(tkinter.Tk):
         
  
 if __name__ == "__main__":
-    app = teste_tk(None)
-    app.title('Teste')
-    app.geometry('1000x700') #ativado para testes
-    #app.resizable(width=False, height=False)
+    root = tkinter.Tk()
+    root.rowconfigure(0, weight=1)
+    root.columnconfigure(0, weight=1)
+    root.title('Teste')
+    
+    #root.geometry('1000x700') #ativado para testes
+    #root.resizable(width=False, height=False)
+    
+    app = teste_tk(root)
     app.mainloop()
